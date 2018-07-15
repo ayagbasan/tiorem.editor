0dfdf468efabc49ee95521cd999a4b586e2ac23c
Ahmet YAĞBASAN <ahmetyagibasan@gmail.com>
2018-07-15 15:54:21 +0300

    ok

--- controller/settingsController.js
+++ controller/settingsController.js
@@ -6,35 +6,61 @@
     $scope.settings = null;
 
     let serviceURL = $rootScope.ENVIRONMENT.WebServiceURL + "config/5b47145574d6642da4c98361";
-
-
-    CallServiceFactory.get("config/5b47145574d6642da4c98361", { token: $rootScope.USER.password })
+    $scope.jobs = [];
+    $scope.global = null;
+    CallServiceFactory.get("config/5b47145574d6642da4c98361?token=" + $rootScope.USER.password)
         .then(function (data) {
 
             if (!data.IsSuccess) {
                 app.ToatsErrorResponse(data)
             }
             else {
-
-
-                $scope.settings = data.Data;
+                $scope.fillJobs(data);
             }
 
         }).catch(function (err) {
             app.ToatsError("Token not valid. " + err, );
         });
 
+    $scope.fillJobs = function (data) {
+        $scope.jobs = [];
+        $scope.global = data.Data;
+        $scope.jobs.push(data.Data.WebHose);
+        $scope.jobs.push(data.Data.GoogleRSS);
+        $scope.jobs.push(data.Data.RssSources);
+        $scope.jobs.push(data.Data.Mapping);
+        $scope.jobs.push(data.Data.Translate);
 
+        
+    }
 
-    $scope.update = function () {
-        CallServiceFactory.put("config/5b47145574d6642da4c98361", { token: $rootScope.USER.password, Data: $scope.settings  })
+    $scope.update = function (job) {
+
+        CallServiceFactory.put("config/5b47145574d6642da4c98361", { token: $rootScope.USER.password, Data: $scope.global })
             .then(function (data) {
 
                 if (!data.IsSuccess) {
                     app.ToatsErrorResponse(data)
                 }
                 else {
+                    app.ToatsSuccess("Update completed")
+                    $scope.fillJobs(data);
+                }
 
+            }).catch(function (err) {
+                app.ToatsError("Token not valid. " + err, );
+            });
+    }
+
+
+    $scope.serviceStatus = function (job, term) {
+        CallServiceFactory.post("config/serviceStatus", { token: $rootScope.USER.password, job:job, Term: term })
+            .then(function (data) {
+
+                if (!data.IsSuccess) {
+                    app.ToatsErrorResponse(data)
+                }
+                else {
 
                     app.ToatsSuccess("Update completed")
                 }
@@ -45,24 +71,6 @@
     }
 
 
-    $scope.serviceStatus = function (serviceName, term) {
-        CallServiceFactory.post("config/webHose", { token: $rootScope.USER.password, ServiceName: serviceName, Term: term })
-            .then(function (data) {
-
-                if (!data.IsSuccess) {
-                    app.ToatsErrorResponse(data)
-                }
-                else {
-                    
-                    app.ToatsSuccess("Update completed")
-                }
-
-            }).catch(function (err) {
-                app.ToatsError("Token not valid. " + err, );
-            });
-    }
-
-   
 
 
 
